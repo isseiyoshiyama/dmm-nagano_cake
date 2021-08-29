6e63225dc1e79a52dd708df9635bc6b7218e8c1d
isseiyoshiyama <issei.1229@gmail.com>
2021-08-29 03:14:37 +0000

    :tada:create top page

--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,13 @@ Rails.application.routes.draw do
     passwords:     'admins/passwords',
     registrations: 'admins/registrations'
   }
-  
+
   devise_for :customers, controllers: {
       sessions:      'customers/sessions',
       passwords:     'customers/passwords',
       registrations: 'customers/registrations'
     }
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
+
+  root to: 'homes#top'
+
 end
