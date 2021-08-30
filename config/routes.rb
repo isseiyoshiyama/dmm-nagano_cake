87cd2bb5b0b51bf7e941e6b60b96e820ece8f30c
isseiyoshiyama <issei.1229@gmail.com>
2021-08-30 14:09:19 +0000

    :tada:create_public/orders_controller&view

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,29 @@
 Rails.application.routes.draw do
-  
+
   scope module: :public do
     get "/customers/my_page" => "customers#show"
     get "/customers/edit" => "customers#edit"
     patch "/customers" => "customers#update"
     get "/customers/unsubscribe" => "customers#unsubscribe"
     patch "/customers/withdraw" => "customers#withdraw"
-    
+
     resources :items, only:[:index, :show]
-    
+
     get "/cart_items" => "cart_items#index"
     patch "/cart_items/:id" => "cart_items#update"
     delete "/cart_items/:id" => "cart_items#destroy"
     delete "/cart_items/destroy_all" => "cart_items#destroy_all"
     post "/cart_items" => "cart_items#create"
+
+    get "/orders/new" => "orders#new"
+    post "/orders/confirm" => "orders#confirm"
+    get "/orders/complete" => "orders#complete"
+    post "/orders" => "orders#create"
+    get "/orders" => "orders#index"
+    get "/orders/:id" => "orders#show"
   end
-  
-  
+
+
 
 
 
