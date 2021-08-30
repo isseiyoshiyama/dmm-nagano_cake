3b148ef74dfb5425707de9de0acd0cf4a1025997
isseiyoshiyama <issei.1229@gmail.com>
2021-08-30 13:57:40 +0000

    :tada:create_public/cart_items_controller&view

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,12 @@ Rails.application.routes.draw do
     patch "/customers/withdraw" => "customers#withdraw"
     
     resources :items, only:[:index, :show]
+    
+    get "/cart_items" => "cart_items#index"
+    patch "/cart_items/:id" => "cart_items#update"
+    delete "/cart_items/:id" => "cart_items#destroy"
+    delete "/cart_items/destroy_all" => "cart_items#destroy_all"
+    post "/cart_items" => "cart_items#create"
   end
   
   
