1f8528409753c50a1e6a73b167c7fecd051b7685
isseiyoshiyama <issei.1229@gmail.com>
2021-09-02 12:21:50 +0000

    :tada:create_admin/order_items_controller&view

--- config/routes.rb
+++ config/routes.rb
@@ -52,5 +52,6 @@ Rails.application.routes.draw do
     resources :genres, only:[:index, :create, :edit, :update]
     resources :customers, only:[:index, :show, :edit, :update]
     resources :orders, only:[:show, :update]
+    resources :order_items, only:[:update]
   end
 end
