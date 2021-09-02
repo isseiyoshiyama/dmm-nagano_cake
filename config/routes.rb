6c8fb0190e42e932eae9eea0b5de7895e617e489
isseiyoshiyama <issei.1229@gmail.com>
2021-09-02 12:05:39 +0000

    :tada:create_admin/genres_controller&view

--- config/routes.rb
+++ config/routes.rb
@@ -49,5 +49,6 @@ Rails.application.routes.draw do
 
   namespace :admin do
     resources :items, only:[:index, :new, :create, :show, :edit, :update]
+    resources :genres, only:[:index, :create, :edit, :update]
   end
 end
