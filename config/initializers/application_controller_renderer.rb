50c8fa0e0ac34a6810e236011c2a74a032207c86
isseiyoshiyama <issei.1229@gmail.com>
2021-09-13 13:40:38 +0000

    :tada:admin_items/:id_show

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '217dd2b7333f8767c7e5dac6f71f784ed6d0c48785f2bbb5dca50c585fc01b09fcd42433ea268821c5d23a1f0f0d1ce4f1e7dd120a8eacebd7ed410ee311ab27'
