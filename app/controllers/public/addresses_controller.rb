87d46ce9c6c54d989795c7259c227eb25e7809a2
isseiyoshiyama <issei.1229@gmail.com>
2021-09-18 08:24:35 +0000

    :tada:customers_addresses/:id/edit

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -14,9 +14,13 @@ class Public::AddressesController < ApplicationController
 
 
   def edit
+    @address = Address.find(params[:id])
   end
 
   def update
+    @address = Address.find(params[:id])
+    @address.update(address_params)
+    redirect_to addresses_path
   end
 
   def destroy
