8abfa0dfe26b0fade753f6b7ac2fe2f7125c10b8
isseiyoshiyama <issei.1229@gmail.com>
2021-10-03 13:25:49 +0000

    :tada:/orders/:id_show

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -58,6 +58,9 @@ class Public::OrdersController < ApplicationController
   end
 
   def show
+    @order = Order.find(params[:id])
+    @order_items = @order.order_items
+
   end
 
   private
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,9 +1,11 @@
 class Order < ApplicationRecord
-  
+
   belongs_to :customer
   has_many :items
   has_many :order_items
-  
+
+
+
   enum payment_method: { credit_card: 0, transfer: 1 }
-  
+
 end
